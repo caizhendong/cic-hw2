c1c7ad6eb86354be457bd7ee220d2acb965cebc6
Zhendong Cai <zhendong.cai@sap.com>
2024-03-11 17:27:11 +0800

    finish homework

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     os.environ["OPENAI_API_TYPE"] = "azure"
     os.environ["OPENAI_API_VERSION"] = "2023-05-15"
     os.environ["OPENAI_API_BASE"] = "https://pvg-azure-openai-uk-south.openai.azure.com/openai"
-    env_path = os.getenv("HOME") + "/Documents/src/openai/.env"
+    env_path = os.getenv("HOME") + "/.env"
 
     st.title('IRM Review')
 
